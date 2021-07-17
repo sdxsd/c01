bdffa87571df9eb1811507f15e1f6402668491c7
sdxsd <willmaguire@icloud.com>
2021-07-17 14:21:41 +0200

    commit

--- ex07/ft_rev_int_tab.c
+++ ex07/ft_rev_int_tab.c
@@ -6,10 +6,11 @@
 /*   By: wmaguire <wmaguire@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/07/13 10:31:19 by wmaguire      #+#    #+#                 */
-/*   Updated: 2021/07/13 10:32:13 by wmaguire      ########   odam.nl         */
+/*   Updated: 2021/07/17 14:21:20 by wmaguire      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 void	ft_rev_int_tab(int *tab, int size);
 
 void	ft_rev_int_tab(int *tab, int size)
